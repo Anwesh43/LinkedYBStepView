3b6e9759fff9bb4edafe74d2045ffb4af615f3b7
Anwesh43 <anweshthecool0@gmail.com>
2018-11-20 10:31:33 +0530

    adding Animator class

--- ybstepview/src/main/java/com/anwesh/uiprojects/ybstepview/YBStepView.kt
+++ ybstepview/src/main/java/com/anwesh/uiprojects/ybstepview/YBStepView.kt
@@ -103,5 +103,33 @@ class YBStepView(ctx : Context) : View(ctx) {
                 cb()
             }
         }
-    } 
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false 
+            }
+        }
+    }
 }
